8525a9c4852d2a961d27b3a18d706bfe6076007d
AndriiKanevskyi <68244465+AndriiKanevskyi@users.noreply.github.com>
2023-01-25 12:31:18 +0100

    HW3

---
+++ src/HW_3.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+/*
+Задание 3
+
+Минимальное из N чисел(использовать LinkedList):
+
+1.       Ввести с клавиатуры число N.
+
+2.       Считать N целых чисел и заполнить ими список - метод getIntegerList.
+
+3.       Найти минимальное число среди элементов списка - метод getMinimum.
+ */
+public class HW_3 {
+    public static LinkedList getIntegerList (int n){
+        Scanner scanner = new Scanner(System.in);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            System.out.println("введите целоє число");
+        linkedList.add(scanner.nextInt());
+        }
+        return linkedList;
+    }//getIntegerList
+
+    public static int getMinimum (LinkedList list){
+        Collections.sort(list);
+        return (int) list.peekFirst();
+    }
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число N");
+        int n = scanner.nextInt();
+        System.out.println("минимальное число из введеных :");
+        System.out.println(getMinimum(getIntegerList(n)));
+
+    }//main
+}//class
